77101044c5c86c55aa5b0ec0be1bd736fce0b81f
Vaishak Menon <vaishakkmenon@gmail.com>
2025-02-28 14:32:09 -0600

    updated project to use vite, react, and typescript: FRESH START

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to My Vite + React + TypeScript App</h1>
+      <p>This is a fresh start!</p>
+    </div>
+  );
+};
+
+export default App;
