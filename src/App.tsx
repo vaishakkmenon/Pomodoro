907cfec1cf9fad5a5c922531e8467c8e7ba2d2cb
Vaishak Menon <vaishakkmenon@gmail.com>
2025-08-27 15:28:45 -0400

    update files to use proper tailwind logic, removed output.css and app.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <main className="min-h-screen grid place-items-center bg-gray text-white">
+    <main className="min-h-screen grid place-items-center text-white">
       <div className="max-w-[1280px] mx-auto p-8 text-center">
         <h1 className="text-3xl font-bold mb-4">Tailwind v4 + Vite + React + TS 🎉</h1>
         <p className="text-lg text-gray-300">
